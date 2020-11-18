275d5d70ddbf381e5cfbe7407a24c6ed076bbc06
sahanna007 <sahannananda@gmail.com>
2020-11-18 18:58:42 +0530

    array

--- Bird.js
+++ Bird.js
@@ -2,11 +2,24 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.trajectory=[]
+    this.pic = loadImage("sprites/smoke.png");
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
+    if (this.body.position.x>200 && this.body.speed>10){
+      var position=[this.body.position.x, this.body.position.y]
+    
+    this.trajectory.push(position)
+    }
+  
+    for(var i=0;i<this.trajectory.length;i=i+1){
+      console.log(this.trajectory[i][0]+"  "+this.trajectory[i][1])
+      image(this.pic,this.trajectory[i][0],this.trajectory[i][1])
+    }
+    
 
     super.display();
   }
--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var gamestate;
 
 
 function preload() {
@@ -41,6 +42,8 @@ function setup(){
 
     //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
+
+    gamestate= "onsling"
 }
 
 function draw(){
@@ -69,16 +72,20 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gamestate=== "onsling"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
 
 
 function mouseReleased(){
-    slingshot.fly();
+    gamestate="launched"
+       slingshot.fly();
 }
 
 function keyPressed(){
     if(keyCode === 32){
+        gamestate="onsling"
         slingshot.attach(bird.body);
     }
 }
